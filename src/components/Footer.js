3f31d03e5028f24aae33a5e9304fa771071e3860
Paul Clifford <clifford@queensu.ca>
2018-10-11 16:37:50 -0400

    styling updates, normalize

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,24 +6,27 @@ const date = new Date()
 
 const Footer = () => (
   <footer id="site-footer">
-    <p className="colophon">
-      © Copyright {date.getFullYear() + ' '}
-      <Link to="/" title="Home">
-        Queen's University Library
-      </Link>
-    </p>
-    <p>
-      Queen's University Library is committed to an inclusive library community
-      with accessible goods, services and facilities that respect the dignity
-      and independence of persons with disabilities.
-    </p>
-    <p>
-      Queen's University is situated on traditional Anishinaabe and
-      Haudenosaunee Territory
-    </p>
-    <p className="queens-logo">
+    <div className="colophon">
+      <p>
+        © Copyright {date.getFullYear() + ' '}
+        <Link to="/" title="Home">
+          Queen's University Library
+        </Link>
+      </p>
+      <p>
+        Queen's University Library is committed to an inclusive library
+        community with accessible goods, services and facilities that respect
+        the dignity and independence of persons with disabilities.
+      </p>
+      <p>
+        Queen's University is situated on traditional Anishinaabe and
+        Haudenosaunee Territory
+      </p>
+    </div>
+
+    <div className="queens-logo">
       <img src={queensLogo} width="200" alt="Queen's University" />
-    </p>
+    </div>
   </footer>
 )
 
