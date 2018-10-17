c2d8b08d877d47833cec61ce4981d2de87a51d0e
Paul Clifford <clifford@queensu.ca>
2018-10-17 11:02:07 -0400

    footer typo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,16 +6,14 @@ const date = new Date()
 
 const Footer = () => (
   <footer id="site-footer">
+  
     <div className="colophon">
       <p>© Copyright {date.getFullYear()} <strong>Queen's University Library</strong></p>
       <p>
-        Queen's University Library is committed to an inclusive library
-        community with accessible goods, services and facilities that respect
-        the dignity and independence of persons with disabilities.
+        Queen's University Library is committed to an inclusive library community with accessible goods, services and facilities that respect the dignity and independence of persons with disabilities.
       </p>
       <p>
-        Queen's University is situated on traditional Anishinaabe and
-        Haudenosaunee Territory
+        Queen's University is situated on traditional Anishinaabe and Haudenosaunee Territory.
       </p>
     </div>
 
