fea96c6ec78d010823d3d690b5f57888b1a9bd0d
Paul Clifford <clifford@queensu.ca>
2018-10-18 16:12:28 -0400

    added Accordian component

---
+++ src/components/Accordian.jsx
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react'
+import posed from 'react-pose'
+import { FaAngleUp } from 'react-icons/fa'
+import { FaAngleDown } from 'react-icons/fa'
+
+const Content = posed.section({
+  closed: { height: 0 },
+  open: { height: 'auto' },
+})
+
+class Accordian extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      open: false,
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="accordian">
+          <span
+            className={this.state.open ? 'title open' : 'title closed'}
+            onClick={() => this.setState({ open: !this.state.open })}
+          >
+            {this.props.title}
+            <FaAngleUp className="open-icon"/>
+            <FaAngleDown className="closed-icon"/>
+          </span>
+
+          <Content
+            className="content"
+            // className={this.state.open ? 'content open' : 'content closed'}
+            pose={this.state.open ? 'open' : 'closed'}
+          >
+            <div>{this.props.children}</div>
+          </Content>
+        </div>
+      </Fragment>
+    )
+  }
+}
+
+export default Accordian
