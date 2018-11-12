9023aa39632728df1db3b77f8a066fea966389d2
Paul Clifford <clifford@queensu.ca>
2018-11-12 16:30:08 -0500

    basic site search styling

--- src/components/Header/SiteSearch.jsx
+++ src/components/Header/SiteSearch.jsx
@@ -1,35 +1,66 @@
 import React from 'react'
-// import { Input } from 'antd'
+import { Input } from 'antd'
 import { FaSearch, FaTimes } from 'react-icons/fa'
+
+const Search = Input.Search
 
 class SiteSearch extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { isToggleOn: true }
 
-    // This binding is necessary to make `this` work in the callback
+    this.state = {
+      searchIsActive: false,
+    }
+
+    this.textInput = React.createRef()
     this.handleClick = this.handleClick.bind(this)
+    this.onFocus = this.onFocus.bind(this)
+    this.onBlur = this.onBlur.bind(this)
+  }
+
+  handleClick() {
+    console.log('button clicked')
+    // this.setState(state => ({
+    //   searchIsActive: !state.searchIsActive,
+    // }))
+
+    this.textInput.current.focus()
+
+  }
+
+  onFocus() {
+    console.log('focused')
+    this.setState({ searchIsActive: true })
+  }
+
+  onBlur() {
+    console.log('blurred')
+    this.setState({ searchIsActive: false })
   }
 
   performSearch = value => {
     console.log(value)
   }
 
-  handleClick() {
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn,
-    }))
-  }
-
   render() {
     return (
-      <button className="site-search-btn" onClick={this.handleClick}>
-        {this.state.isToggleOn ? (
-          <FaSearch className="anticon" />
-        ) : (
-          <FaTimes className="anticon" />
-        )}
-      </button>
+      <>
+        <Search
+          placeholder="Search our site"
+          ref={this.textInput}
+          onSearch={this.performSearch}
+          onFocus={this.onFocus}
+          onBlur={this.onBlur}
+          className={this.state.searchIsActive ? 'active' : null}
+        />
+        <button className="site-search-btn" onClick={this.handleClick}>
+          {this.state.searchIsActive ? (
+            <FaTimes className="anticon" />
+          ) : (
+            <FaSearch className="anticon" />
+          )}
+        </button>
+      </>
     )
   }
 }
--- src/components/Header/UtilMenu.jsx
+++ src/components/Header/UtilMenu.jsx
@@ -5,69 +5,68 @@ import { FaKey, FaComments, FaUser } from 'react-icons/fa'
 
 import SiteSearch from './SiteSearch.jsx'
 
-const MyAccountMenu = (
-  <Menu title="My Account">
-    <Menu.Item key="setting:1">
-      <a href="https://qcat.library.queensu.ca/vwebv/myAccount">
-        My Library Account (QCAT)
-      </a>
-    </Menu.Item>
-    <Menu.Item key="setting:1">
-      <a href="https://racer2.scholarsportal.info">Interlibrary Loan (RACER)</a>
-    </Menu.Item>
-    <Menu.Item key="setting:1">
-      <a href="https://ereserves.library.queensu.ca/ares/">
-        Course eReserves (Ares)
-      </a>
-    </Menu.Item>
-    <Menu.Item key="setting:1">
-      <a href="https://onq.queensu.ca">onQ (Learning Management System)</a>
-    </Menu.Item>
-    <Menu.Item key="setting:1">
-      <a href="https://studentweb.housing.queensu.ca/">
-        Tri-Colour Cash (PrintQ)
-      </a>
-    </Menu.Item>
-    <Menu.Item key="setting:1">
-      <a href="https://outlook.com/queensu.ca">Office 365</a>
-    </Menu.Item>
-  </Menu>
-)
-
 class UtilMenu extends React.Component {
+
   rootSubmenuKeys = ['sub1', 'sub2', 'sub3', 'sub4']
 
   render() {
     return (
-      <div>
-        <Menu id="util-nav" mode="inline" selectable={false}>
-          <Menu.Item key="off-campus">
-            <Link to="/">
-              <FaKey className="anticon" />
-              Off-Campus Access
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="ask-us">
-            <Link to="/">
-              <FaComments className="anticon" />
-              Ask Us
-            </Link>
-          </Menu.Item>
-          <Menu.Item key="my-account">
-            <Dropdown overlay={MyAccountMenu} trigger={['click']}>
-              <button className="ant-dropdown-link">
-                <FaUser className="anticon" />
-                My Account
-              </button>
-            </Dropdown>
-          </Menu.Item>
-          <Menu.Item key="search">
-            <SiteSearch />
-          </Menu.Item>
-        </Menu>
-      </div>
+      <Menu id="util-nav" mode="inline" selectable={false}>
+        <Menu.Item key="off-campus">
+          <Link to="/">
+            <FaKey className="anticon" />
+            Off-Campus Access
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="ask-us">
+          <Link to="/">
+            <FaComments className="anticon" />
+            Ask Us
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="my-account">
+          <Dropdown overlay={MyAccountMenu} trigger={['click']}>
+            <button className="ant-dropdown-link">
+              <FaUser className="anticon" />
+              My Account
+            </button>
+          </Dropdown>
+        </Menu.Item>
+        <Menu.Item key="site-search" id="site-search">
+          <SiteSearch/>
+        </Menu.Item>
+      </Menu>
     )
   }
 }
 
 export default UtilMenu
+
+const MyAccountMenu = (
+  <Menu title="My Account">
+    <Menu.Item key="qcat">
+      <a href="https://qcat.library.queensu.ca/vwebv/myAccount">
+        My Library Account (QCAT)
+      </a>
+    </Menu.Item>
+    <Menu.Item key="racer">
+      <a href="https://racer2.scholarsportal.info">Interlibrary Loan (RACER)</a>
+    </Menu.Item>
+    <Menu.Item key="ares">
+      <a href="https://ereserves.library.queensu.ca/ares/">
+        Course eReserves (Ares)
+      </a>
+    </Menu.Item>
+    <Menu.Item key="onq">
+      <a href="https://onq.queensu.ca">onQ (Learning Management System)</a>
+    </Menu.Item>
+    <Menu.Item key="printq">
+      <a href="https://studentweb.housing.queensu.ca/">
+        Tri-Colour Cash (PrintQ)
+      </a>
+    </Menu.Item>
+    <Menu.Item key="o365">
+      <a href="https://outlook.com/queensu.ca">Office 365</a>
+    </Menu.Item>
+  </Menu>
+)
